2c4c458c6b7f938b46c19e856faec2e2d41f1533
karimi <n.karimi@pomtech.com>
2020-11-24 16:04:49 +0430

    Fuel Station Completed

--- resources/js/src/views/apps/inventory/add/FuelStation.vue
+++ resources/js/src/views/apps/inventory/add/FuelStation.vue
@@ -23,15 +23,13 @@
     <vx-card class="mb-5">
       <div class="vx-row justify-between pr-5 pl-5">
         <div class="w-1/3">
-          <label for>
-            <small>تانک تیل</small>
-          </label>
+          <label class="vs-input--label">تانک تیل</label>
           <v-select label="text" :options="itemType" :dir="$vs.rtl ? 'rtl' : 'ltr'" />
         </div>
         <div class="w-1/3">
           <vs-button
             icon-pack="feather"
-            icon="icon-chevrons-left"
+            icon="icon-plus"
             icon-after
             class="shadow-md float-right"
             @click="openFuel()"
@@ -42,15 +40,32 @@
     <vx-card>
       <div class="vx-row">
         <div class="w-1/2 pr-2">
-          <vs-button
-            type="filled"
-            @click="openDespencer()"
-            class="mb-5 block float-right"
-          >ثبت دسپنسر</vs-button>
+          <div class="justify-between">
+            <vs-button type="line" line-origin="right" class="cursor-default">دسپنسرها</vs-button>
+            <vs-button
+              icon-pack="feather"
+              icon="icon-plus"
+              icon-after
+              size="small"
+              @click="openDespencer()"
+              class="mb-5 float-right"
+            >ثبت دسپنسر</vs-button>
+          </div>
           <DespencerList />
         </div>
         <div class="w-1/2 pl-2">
-          <vs-button type="filled" @click="openFuelStore()" class="mb-5 block float-right">ثبت ذخیره</vs-button>
+          <div class="justify-between">
+            <vs-button type="line" line-origin="right" class="cursor-default">ذخیره ها</vs-button>
+
+            <vs-button
+              icon-pack="feather"
+              icon="icon-plus"
+              icon-after
+              size="small"
+              @click="openFuelStore()"
+              class="mb-5 float-right"
+            >ثبت ذخیره</vs-button>
+          </div>
           <FuelStoreList />
         </div>
       </div>
