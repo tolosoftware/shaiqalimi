266757b3e85b4211735102da5ed8d2ae0a0a3c8b
naseri <mahdi.naseri1376@gmail.com>
2020-11-28 11:36:41 +0430

    axios configuration

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,13 +22,15 @@ class ComposerAutoloaderInit52ca459405fa5b51ef0a16cefa2e01e2
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit52ca459405fa5b51ef0a16cefa2e01e2', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit52ca459405fa5b51ef0a16cefa2e01e2', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+            require __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInit52ca459405fa5b51ef0a16cefa2e01e2::getInitializer($loader));
         } else {
