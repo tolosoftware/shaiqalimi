31d2fc7a11e55de70e1fd0e30019d94fc041b52e
naseri <mahdi.naseri1376@gmail.com>
2020-12-28 13:10:56 +0430

    user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,6 +8,7 @@ use App\Models\UserAssignment;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\DB;
 
+
 class UserController extends Controller
 {
     /**
@@ -40,31 +41,43 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-
-       
-   
         // $this->validate($request,[
         //     'name' => 'required|string|max:191',
         //     'email' => 'required|string|email|max:191|unique:users',
         //     'password' => 'required|string|min:6'
         // ]);
 
-        $photoname = time().'.' . explode('/', explode(':', substr($request->image, 0, strpos($request->image, ';')))[1])[1];
-        \Image::make($request->image)->save(public_path('img/user/').$photoname);
-        $request->merge(['photo' => $photoname]);
+        DB::beginTransaction();
+        try {
 
-        return User::create([
-            'firstName' => $request['firstName'],
-            'lastName' => $request['lastName'],
-            'user_type' =>$request['user_type']['value'],
-            'position' => $request['position'],
-            'email' => $request['email'],
-            'phone' => $request['phone'],
-            'address' => $request['address'],
-            'password' => Hash::make($request['password']),
-            'image' => $photoname,
-        ]);
-       
+            $photoname = time().'.' . explode('/', explode(':', substr($request->image, 0, strpos($request->image, ';')))[1])[1];
+            \Image::make($request->image)->save(public_path('img/user/').$photoname);
+            $request->merge(['photo' => $photoname]);
+
+            $user = User::create([
+                'firstName' => $request['firstName'],
+                'lastName' => $request['lastName'],
+                'user_type' =>$request['user_type']['value'],
+                'position' => $request['position'],
+                'email' => $request['email'],
+                'phone' => $request['phone'],
+                'address' => $request['address'],
+                'password' => Hash::make($request['password']),
+                'image' => $photoname,
+            ]);
+
+            if($request->userleaders != null){
+                foreach ($request->userleaders as $key => $val) {
+                    UserAssignment::create(['lead_user_id' => $val['id'], 'user_id'=> $user->id]);
+                }
+            }
+
+        DB::commit();
+        return ['msg' => 'user successfully inserted'];
+    } catch (Exception $e) {
+        DB::rollback();
+    }
+        
     }
 
     /**
