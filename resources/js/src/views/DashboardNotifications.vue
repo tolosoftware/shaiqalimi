7072a662e62b9f57b9319801dd15a7fec872fef0
karimi <nak.73406@gmail.com>
2021-01-11 13:58:29 +0430

    load the notifcation

--- resources/js/src/views/DashboardNotifications.vue
+++ resources/js/src/views/DashboardNotifications.vue
@@ -5,7 +5,7 @@
       <div class="vx-row">
         <!-- TITLE COLOR -->
         <div v-for="item in ImportnatNotif" :key="item.id" class="vx-col w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/3 mb-base">
-          <vx-card class="pt-2" :title="item.title" :title-color="item.type" :subtitle="item.subtitle" :time="moment([item.gen_date]).fromNow()" :content-color="item.contentColor" :subtitle-color="item.subtitleColor" :card-background="item.cardBackground">
+          <vx-card :title="item.title" :title-color="item.type" :subtitle="item.subtitle" :time="moment([item.gen_date]).fromNow()" :content-color="item.contentColor" :subtitle-color="item.subtitleColor" :card-background="item.cardBackground">
             <span class="item-time-ago cursor-pointer">
               <!--<feather-icon icon="BookmarkIcon" svgClasses="w-5 h-6 text-success" />-->
               <vs-button radius color="primary" size="small" svgClasses="w-2 h-3 hover:text-danger" type="border" icon="close" style="margin-right:7px;" @click="showWarn()"></vs-button>&nbsp;&nbsp;
@@ -28,7 +28,7 @@
       <div class="vx-row">
         <!-- TITLE COLOR -->
         <div v-for="item in allNotif" :key="item.id" class="vx-col w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/3 mb-base cursor-pointer">
-          <vx-card class="pt-2" :title="item.title" :title-color="item.type" :subtitle="item.subtitle" :time="moment([item.gen_date]).fromNow()" :content-color="item.contentColor" :subtitle-color="item.subtitleColor" :card-background="item.cardBackground">
+          <vx-card :title="item.title" :title-color="item.type" :subtitle="item.subtitle" :time="moment([item.gen_date]).fromNow()" :content-color="item.contentColor" :subtitle-color="item.subtitleColor" :card-background="item.cardBackground">
             <span class="item-time-ago" :style="'color:' + item.titleColor">
               <!-- <feather-icon icon="CornerRightUpIcon" svgClasses="w-5 h-6 text-success"></feather-icon>&nbsp;&nbsp;
               <vs-button radius color="primary" type="border" icon="search"></vs-button>
